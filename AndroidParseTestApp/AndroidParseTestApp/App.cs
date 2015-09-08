af2b9a1317be4ea398dac3e64292380521db6f24
Seth <sethplunkett@gmail.com>
2015-09-08 14:36:48 -0700

    pushing test app

---
+++ AndroidParseTestApp/AndroidParseTestApp/App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using Parse;
+
+namespace ParseAndroidStarterProject
+{
+	[Application]
+	public class App : Application
+	{
+		public App (IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+		{
+		}
+
+		public override void OnCreate ()
+		{
+			base.OnCreate ();
+
+			// Initialize the Parse client with your Application ID and .NET Key found on
+			// your Parse dashboard
+			ParseClient.Initialize ("kOJRWgMgXY4tdecrmOnGNJ7UvRrF5BQ26anwESpj", "3fDyL2ynxVk1bZF9SzARrLRPY84SFDufAoY95QpV");
+		}
+	}
+}
---
+++ AndroidParseTestApp/AndroidParseTestApp/MainActivity.cs
@@ -0,0 +1,86 @@
+﻿ using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Parse;
+using System.Collections.Generic;
+
+namespace AndroidParseTestApp
+{
+	[Activity (Label = "AndroidParseTestApp", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+//		int count = 1;
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+		
+
+//			Get our button from the layout resource,
+// 			and attach an event to it
+
+//			var btn = this.FindViewById<Button> (Resource.Id.Button);
+//			btn.Click += async (sender, e) => {
+//				ParseQuery<ParseObject> query = ParseObject.GetQuery ("TestClass");
+//				ParseObject object1 = await query.GetAsync ("HQSr8bt1Zp");
+//				string TestName = object1.Get<string> ("TestName");
+//				btn.Text = string.Format ("{0}", TestName);
+//			};
+//		
+			EditText edittext = FindViewById<EditText>(Resource.Id.edittext);
+
+			edittext.KeyPress += async (object sender, View.KeyEventArgs e) =>{
+				e.Handled = false;
+				if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter)
+				{
+					ParseQuery<ParseObject> query = ParseObject.GetQuery ("TestClass");
+					ParseObject object1 = await query.GetAsync (string.Format("{0}", edittext.Text));
+					string TestName = object1.Get<string> ("TestName");
+//					btn.Text = string.Format ("{0}", TestName);
+					Toast.MakeText(this, string.Format ("{0}", TestName), ToastLength.Long).Show();
+					e.Handled = true;
+				}
+			};
+
+//			var btn = this.FindViewById<Button>(Resource.Id.Button);
+//			btn.Click += async (sender, e) => {
+//				var query = ParseObject.GetQuery("TestClass")
+//					.WhereNotEqualTo("TestName", "The Vintage");
+//				
+//					
+//				btn.Text = string.Format ("{0}", query);
+//			};
+
+
+
+//			how to print
+//			System.Diagnostics.Debug.WriteLine ();
+
+
+//			create a new user
+
+//			var user = new ParseUser ()
+//			{
+//				Username = "my name",
+//				Password = "my pass",
+//				Email = "email@example.com"
+//			};
+//
+//			// other fields can be set just like with ParseObject
+//			user ["phone"] = "415-392-0202";
+//
+//			await user.SignUpAsync ();
+
+		}
+	}
+}
+
+
